98ab3eef4c722525d3eaff2fd4f1677cfe80fdba
Kent@DESKTOP-92HDSMQ <[kent831217@gmail.com]>
2021-10-13 23:24:44 +0800

    1013 監聽開關

--- cmds/time.py
+++ cmds/time.py
@@ -1,40 +1,30 @@
 import datetime
 import discord
 from discord.ext import commands
-import json, asyncio ,datetime
-from  ig_crawler import igcr
+import json, asyncio, datetime
+from ig_crawler import igcr
 
 with open('setting.json', mode='r', encoding='utf8') as jFile:
     jdata = json.load(jFile)
 
+
 class time(commands.Cog):
-    def __init__(self,bot,*args,**kwargs):
+    def __init__(self, bot, *args, **kwargs):
+        self.is_listening: bool = False;
         self.bot = bot
         with open('setting.json', mode='r', encoding='utf8') as jFile:
             jdata = json.load(jFile)
+
         async def interval():
             count = 0
             await self.bot.wait_until_ready()
             # self.channel = self.bot.get_channel(int(jdata["Channel1"]))
             while not self.bot.is_closed():
                 self.channel = self.bot.get_channel(int(jdata["channel_poster"]))
-                time_now= datetime.datetime.now().strftime("%H%M")
+                time_now = datetime.datetime.now().strftime("%H%M")
                 await self.channel.send(f"Now is {time_now}")
 
-                _igcr = igcr.Singleton()
-                print(_igcr.islogin)
-                if(_igcr.islogin==True):
-                    aa = jdata["Ig"]
-                    for username in aa:
-                        print(username+" start...")
-                        newpost = _igcr.sendNewPost(username)
-                        for i in newpost:
-                            pic = discord.File(i)
-                            await self.channel.send(file=pic)
-
-                # if time_now == jdata["time"]:
-                #     await self.channel.send("HIHI,TIME")
-                await asyncio.sleep(10)  # 單位:秒
+                await asyncio.sleep(60)  # 單位:秒
 
         self.bg_task = self.bot.loop.create_task(interval())
 
@@ -46,6 +36,29 @@ class time(commands.Cog):
         with open("setting.json", mode="w", encoding="utf8") as jFile:
             json.dump(jdata, jFile, indent=4)
             await ctx.send(f"Set complete,Time is {jdata['time']} now")
+
+    @commands.command()
+    async def golisten(self, ctx):
+        _igcr = igcr.Singleton()
+        self.is_listening = True
+        while not self.bot.is_closed() and self.is_listening:
+            print(_igcr.islogin)
+            if (_igcr.islogin == True):
+                aa = jdata["Ig"]
+                for username in aa:
+                    print(username + " start...")
+                    await ctx.send(username + " start...")
+                    newpost = _igcr.sendNewPost(username)
+                    for i in newpost:
+                        pic = discord.File(i)
+                        await ctx.send(file=pic)
+            await asyncio.sleep(60)  # 單位:秒
+
+    @commands.command()
+    async def stoplisten(self, ctx):
+        self.is_listening = False
+        print("is_listening : "+str(self.is_listening))
+        await ctx.send("is_listening : "+str(self.is_listening))
 
 
 def setup(bot):
